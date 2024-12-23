25e40d279b0b98ad14426f87303164067e28e7a7
marvins <enujekormarvellous@gmail.com>
2024-12-23 08:32:36 +0100

    added database postgress

--- instance/config.py
+++ instance/config.py
@@ -3,7 +3,7 @@ SECRET_KEY= 'i am a very random strin 923280'
 APP_NAME='flaskproject'
 DEBUG=True
 ENV='production'
-SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root:@localhost/perfume_website'
+SQLALCHEMY_DATABASE_URI='postgresql://best_perfume_user:qV2RF1YASJ5af0eB9LpzXyc8LpEq9frg@dpg-ctkh2pq3esus73e7vmmg-a.oregon-postgres.render.com/best_perfume'
 SQLALCHEMY_TRACK_MODIFICATIONS=True
 
 MAIL_SERVER="smtp.gmail.com"
